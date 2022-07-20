4bcf755b35d781b649b62b5ed9c8e487ae221672
Leann Fraoigh <d.myshlennikova@gmail.com>
2022-07-20 23:16:12 +0300

    Добавляет приватный роут

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -4,7 +4,8 @@ import LoginScreen from '../../pages/login-screen/login-screen';
 import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
 import PropertyScreen from '../../pages/property-screen/property-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
-import { AppRoute } from '../../const';
+import PrivateRoute from '../private-route/private-route';
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 function App(): JSX.Element {
   return (
@@ -12,7 +13,12 @@ function App(): JSX.Element {
       <Routes>
         <Route path={AppRoute.Main} element={<MainScreen placesCounter={666}/>} />
         <Route path={AppRoute.Login} element={<LoginScreen/>} />
-        <Route path={AppRoute.Favorites} element={<FavoritesScreen/>} />
+        <Route path={AppRoute.Favorites} element={
+          <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+            <FavoritesScreen/>
+          </PrivateRoute>
+        }
+        />
         <Route path={AppRoute.Room} element={<PropertyScreen/>} />
         <Route path='*' element={<NotFoundScreen/>} />
       </Routes>
